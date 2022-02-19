ed1066e0822c304c431e76c677865a06517be7a4
cyriel loic <meliyamtcheuloic@gmail.com>
2022-02-19 23:30:12 +0100

    update API gestion notes

--- Models/enseignant.model.js
+++ Models/enseignant.model.js
@@ -6,7 +6,7 @@ const schema = mongoose.Schema({
     prenom: {type: String, default: ""},
     email: {type: String, default: ""},
     password: {type: String, default: ""},
-    matieres: [{ type: mongoose.Schema.Types.ObjectId, ref:'matiere' }], 
+    matiere: {type: [Object], default: [] } 
     
 })
 
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const session = require('express-session');
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true})
 mongoose.connection.on('open', () => {
@@ -18,9 +20,45 @@ const Modules = require('./Models/module.model')
 const Matieres = require('./Models/matiere.model')
 const Enseignants = require('./Models/enseignant.model')
 
+const MAX_AGE = 1000 * 60 * 60 * 3; // Three hours
+const mongoDBstore = new MongoDBStore({
+  uri: 'mongodb://localhost:27017/test',
+  collection: "mySessions"
+});
+
 
 server.use(cors())
 server.use(bodyParser.json())
+
+/*
+server.use(session({ secret: 'keyboard cat',
+resave: true,
+saveUninitialized: false,
+cookie: { maxAge: 60000, secure:false }
+}));
+
+*/
+
+const IS_PROD = false
+const COOKIE_NAME = 'keyboard'
+const SESS_SECRET = 'keyboard cat'
+// Express-Session
+server.use(
+  session({
+    name: COOKIE_NAME, //name to be put in "key" field in postman etc
+    secret: SESS_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: mongoDBstore,
+    cookie: {
+      maxAge: 60000,
+      sameSite: false,
+      secure: IS_PROD
+    }
+  })
+)
+
+
 
 
 /* ******************************** LOGIN & INSCRIPTION ETUDIANT ********************************************** */
@@ -28,20 +66,50 @@ server.use(bodyParser.json())
 
 server.post('/signin', async function (req, res) {
     const elem = req.body
+    console.log(elem)
+    if (!elem.email || !elem.password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+
+    //check for existing user
     const infos = await Etudiants.findOne({email: elem.email})
     console.log(infos)
+    // Validate password
     if(infos && bcrypt.compareSync(elem.password, infos.password)){
       console.log("le mot de passe correspond")
       const status = infos.acknowledged ? 201 : 400
-      res.writeHead(status)
-      res.end()
+      
+      const sessUser = { id: infos._id, nom: infos.nom, email: infos.email, prenom: infos.prenom };
+      req.session.Etudiant = sessUser; // Auto saves session data in mongo store
+
+      res.json({ msg: " Logged In Successfully", sessUser }); // sends cookie with sessionID automatically in response
+   
+    
 
     }
+
+  // res.writeHead(status)
+  // res.end()
+
+  /*
+    
+    User.findOne({ email }).then((user) => {
+    if (!user) return res.status(400).json({ msg: "User does not exist" });
+
+    
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+      const sessUser = { id: user.id, name: user.name, email: user.email };
+      req.session.user = sessUser; // Auto saves session data in mongo store
+
+      res.json({ msg: " Logged In Successfully", sessUser }); // sends cookie with sessionID automatically in response
    
+    */
+
 })
 
 // endpoint pour l'inscription de l"etudiant
-
 server.post('/signup', async function (req, res){
   const elem = req.body
 
@@ -50,6 +118,10 @@ server.post('/signup', async function (req, res){
   const password = bcrypt.hashSync(elem.password, 10);
   console.log(password)
 
+  Etudiants.findOne({ email: elem.email }).then(async (user) => {
+    if (user) return res.status(400).json({ msg: "Etudiant already exists" });
+  
+  // New Etudiant created
   const infos = await Etudiants.create({
     nom:elem.nom,
     prenom: elem.prenom,
@@ -61,11 +133,47 @@ server.post('/signup', async function (req, res){
     notes: elem.notes
   })
 
-  const status = infos._id ? 201 : 400
-  res.writeHead(status)
-  res.end()
+    const status = infos._id ? 201 : 400
+    res.writeHead(status)
+    res.end()
+
+  })
 
 })
+
+// Cette route est utilisée pour acceder à la page d'acceuil de notre application React
+server.get("/authchecker", (req, res) => {
+  const sessUser = req.session.Etudiant
+  if (sessUser) {
+    return res.json({ msg: " Authenticated Successfully", sessUser })
+  } else {
+    return res.status(401).json({ msg: "Unauthorized" })
+  }
+})
+
+// GET logout
+server.get('/logout', function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if(err) {
+        return next(err);
+      } else {
+        return res.redirect('/');
+      }
+    });
+  }
+});
+
+
+server.delete("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    //delete session data from store, using sessionID in cookie
+    if (err) throw err;
+    res.clearCookie("session-id"); // clears cookie containing expired sessionID
+    res.send("Logged out successfully");
+  });
+});
 
 
 /*************************** */
@@ -240,46 +348,96 @@ server.post('/matiere', async function(req, res){
 /* **********************  ENSEIGNANT ******************************/
 
 server.get('/enseignant', async function(req, res){
-  const infos = await Enseignants.find().populate("matieres")
+  const infos = await Enseignants.find().populate("matiere")
   res.json(infos)
   console.log(infos)
 })
 
 
+async function getCourseID(nom) {
+	const myQuery = { nom: nom };
+	const matieres = await Matieres.find(myQuery);
+	//log.info("THIS IS THE COURSE ID", courseID);
+	return matieres[0]._id;
+}
+
+
 server.post('/signupEnseignant', async function(req, res){
   const elem = req.body
   const password = bcrypt.hashSync(elem.password, 10);
-  const matieres = await Matieres.find({nom:elem.matieres})
-  console.log(matieres)
 
+  Enseignants.findOne({ email: elem.email }).then(async (user) => {
+    if (user) return res.status(400).json({ msg: "Enseignant already exists" });
+  
   const infos = await Enseignants.create({
       nom:elem.nom,
       prenom:elem.prenom,
       email: elem.email,
-      password: password,
-      matieres: matieres[0]._id
+      password: password
   })
-  const status = infos._id ? 201 : 400
-  res.writeHead(status)
-  res.end()
 
-})
+  const status = infos._id ? 201 : 400
+
+    if(status == 201){
+      let { email, matieres } = req.body;
+      matieres.forEach(async (matiere) => {
+          const courseID = await getCourseID(matiere);
+          console.log("THIS IS IT", courseID);
+          const myUpdate = { $push: { matiere: courseID } };
+          Enseignants.updateOne({ email: email }, myUpdate, (err, res1) => {
+            if (err) {
+              res.status(500);
+            }
+        
+            })
+          })
+      }
+    res.writeHead(status)
+    res.end()
+
+	    })
+
+  })
+
 
 server.post('/signinEnseignant', async function(req, res){
   const elem = req.body
-  const infos = await Enseignants.findOne({email: elem.email})
+
+  if (!elem.email || !elem.password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
+  
+    //check for existing user
+    const infos = await Enseignants.findOne({email: elem.email})
     console.log(infos)
+
+    // Validate password
     if(infos && bcrypt.compareSync(elem.password, infos.password)){
       console.log("le mot de passe correspond")
       const status = infos.acknowledged ? 201 : 400
-      res.writeHead(status)
-      res.end()
 
-    }
-    
+      const sessUser = { id: infos._id, nom: infos.nom, email: infos.email, prenom: infos.prenom };
+      req.session.Enseignant = sessUser; // Auto saves session data in mongo store
+
+      res.json({ msg: " Logged In Successfully", sessUser }); // sends cookie with sessionID automatically in response
+   
+     // res.writeHead(status)
+     // res.end()
+
+    } 
 })
 
 
+// Cette route est utilisée pour acceder à la page d'acceuil de notre application React
+server.get("/authcheckerEns", (req, res) => {
+  const sessUser = req.session.Enseignant
+  if (sessUser) {
+    return res.json({ msg: "Authenticated Successfully", sessUser })
+  } else {
+    return res.status(401).json({ msg: "Unauthorized" })
+  }
+
+})
 
 
 
